647c6df095efe1217890ce178dd1b5e1d1724a47
Ale Candia <alejandrogcandia@gmail.com>
2023-09-15 14:04:10 -0300

    se crea componente TableForm, y el loader

--- src/component/DashHome/DashHome.jsx
+++ src/component/DashHome/DashHome.jsx
@@ -1,16 +1,20 @@
 import { Box } from '@mui/material';
 import styles from './DashHome.module.css'
-
-
+import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import TableForms from '../TableForms/TableForms.jsx';
+import LoaderDash from '../LoaderDash/LoaderDash.jsx';
 
 export default function DashHome() {
+const {user,forms} = useSelector(state=>state.data)
 
- 
                                              
   return (
     <Box className={styles.dashhome}>
       <h1>DashHome</h1>
     
+      {forms&&<TableForms forms={forms}/>}
+     {(!forms&&user)&&<LoaderDash/>} 
     </Box>
   );
 }
---
+++ src/component/TableForms/TableForms.jsx
@@ -0,0 +1,86 @@
+// TableForms.js
+import { Box, Button, Typography } from "@mui/material";
+import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import React, { useEffect } from "react";
+import {  useSelector } from "react-redux";
+import ListIcon from '@mui/icons-material/List';
+import styles from "./TableForms.module.css";
+import CreateForm from "../CreateForm/CreateForm";
+
+export default function TableForms({ forms }) {
+ const {user} = useSelector(state=>state.data)
+  useEffect(() => {
+   
+    
+  }, [forms]);
+const handleDelete = (id,user)=>{
+// const form = forms.find((e)=>{return e.id===id})
+//  if(form.pending){
+//   deleteFormPending(id,user)
+//   alertDeleteFormPending();
+//  }else{
+//   alertErrorDeleteFormPending();
+//  }
+
+
+}
+  return (
+    <Box className={styles.tablecontainer}>
+    
+        {forms.length?<>
+        <Box sx={{display:'flex'}}>
+               <ListIcon/>
+               <Typography variant="body1" component="p" className={styles.tabletitle}>
+        Tus formularios</Typography>
+        </Box>
+        <Box sx={{width:'100%'}}>
+        <CreateForm/>
+      </Box>
+      <table className={styles.table}>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Pending</th>
+            <th>Creation date</th>
+            <th>Delete</th>
+          </tr>
+        </thead>
+        <tbody>
+          {forms.map((form) => {
+          
+            return (
+              <tr
+              key={form.id}
+                className={form.pending ? styles.pendingRow : null}
+                >
+                <td>
+                
+                    {form.id}
+                  
+                </td>
+                <td>{form.pending ? "Yes" : "No"}</td>
+                <td>{form.createdAt.split("T")[0]}</td>
+                <td><Button onClick={()=>{handleDelete(form.id,user)}}><DeleteForeverIcon/></Button></td>
+              </tr>
+            )
+          })}
+        </tbody>
+          
+      </table> </> :null
+     }
+     {!forms.length  ?<>
+      <Box sx={{width:'100%'}}>
+      <Box sx={{width:'100%'}}>
+        <CreateForm/>
+        
+      </Box>
+      <Box sx={{display:'flex',justifyContent:'center',alignItems:'center',flexDirection:'column'}}>
+      <Typography>No tienes formularios en la base de datos</Typography>
+      <Typography>Para crear un formulario nuevo, haz click en el botón del signo más.</Typography>
+      </Box>
+      </Box>
+     </>:null}
+     
+    </Box>
+  );
+}
