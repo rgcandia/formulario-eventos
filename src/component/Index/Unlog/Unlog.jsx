6f82dc69765fb554b17b99ea5818627a109c4d5b
Ale Candia <alejandrogcandia@gmail.com>
2023-09-19 14:50:31 -0300

    se crea una barra de navegación, dentro del componente unlog, a futuro hay que cambiar de nombre para ese componente

--- src/component/Index/Unlog/Unlog.jsx
+++ src/component/Index/Unlog/Unlog.jsx
@@ -1,8 +1,10 @@
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Button, Typography,AppBar,Toolbar } from "@mui/material";
 import {disconect} from '../../../firebase/auth_sign_out.js'
 import { useDispatch, useSelector } from "react-redux";
 import {uploadUser} from '../../../redux/slice.js'
 import LogoutIcon from '@mui/icons-material/Logout';
+import logo from '../../../images/encabezado-wellspring.png';
+
 import styles from './Unlog.module.css'
 export default function Unlog(){
   const {user} = useSelector(state=>state.data)
@@ -11,21 +13,39 @@ export default function Unlog(){
     disconect(dispatch,uploadUser);
     location.reload()
   }
-    return(<Box className={styles.unlog}>
+    return(<AppBar position="static">
+      <Toolbar>
+  <Box>
+                  <a href="/">
+                    <img
+                    className={styles.logo}
+                    src={logo}
+                    alt={logo.substring(30)}
+                    />
+                    </a>
+  </Box>
+
+      </Toolbar>
+
+    </AppBar>)
+}
+
+
+
+// <Box className={styles.unlog}>
        
        
-       <Box sx={{display:'flex',alignItems:'center',justifyContent:'center'}}>
-        <Typography variant='body2'>Bienvenido : <strong>{user}</strong></Typography>
-       </Box>
+//        <Box sx={{display:'flex',alignItems:'center',justifyContent:'center'}}>
+//         <Typography variant='body2'>Bienvenido : <strong>{user}</strong></Typography>
+//        </Box>
       
-       <Box sx={{display:'flex',alignItems:'center',justifyContent:'center'}}>
-       <Button variant='outlined' color='primary' onClick={handleClick}>
-       <LogoutIcon/>
-       <Typography>logout</Typography>
-        </Button>
-       </Box>
+//        <Box sx={{display:'flex',alignItems:'center',justifyContent:'center'}}>
+//        <Button variant='outlined' color='primary' onClick={handleClick}>
+//        <LogoutIcon/>
+//        <Typography>logout</Typography>
+//         </Button>
+//        </Box>
       
        
      
-    </Box>)
-}
+//     </Box>
